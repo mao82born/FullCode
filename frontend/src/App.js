793a9a255cdd6c9f62f81514218dace08c04e90c
DiegoEdison <diegoedison05@gmail.com>
2022-11-06 14:43:24 -0500

    Instalando Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { HomeScreen } from './screens/HomeScreen';
 import { ProductScreen } from './screens/ProductScreen';
+import './index.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to={'/'}>FullGames</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>FullGames</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:id" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer className="text-center">Todos los derechos reservados</footer>
       </div>
     </BrowserRouter>
   );
