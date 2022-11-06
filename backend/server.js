5fd24db1db8938a11f19b86bcadf63c79d2f7224
DiegoEdison <diegoedison05@gmail.com>
2022-11-06 15:54:11 -0500

    Creción de la página donde muestra los detalles del producto

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/id/:id', (req, res) => {
+  const product = data.products.find((x) => x.id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Producto no encontrado.' });
+  }
+  res.send(data.products);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http;//localhost:${port}`);
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,78 @@
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export function ProductScreen() {
   const params = useParams();
   const { id } = params;
-  return (
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/id/${id}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  return loading ? (
+    <div>Cargando...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <div>
-      <h1>{id}</h1>
+      <Row>
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>Descripción: {product.description}</ListGroup.Item>
+            <ListGroup.Item>Precio: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Stock: {product.countInStock}</ListGroup.Item>
+            <ListGroup.Item>
+              <div className="d-grid">
+                <Button variant="primary">Añadir al carrito</Button>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
     </div>
   );
 }
