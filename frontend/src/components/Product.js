2f1c45d5558775e2362c56ecc0fc00827479064b
DiegoEdison <diegoedison05@gmail.com>
2022-11-06 15:09:39 -0500

    Modificando estilos a los productos

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,21 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+export function Product(props) {
+  const { product } = props;
+  return (
+    <Card>
+      <Link to={`/product/${product.id}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.id}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Añadir al carrito</Button>
+      </Card.Body>
+    </Card>
+  );
+}
