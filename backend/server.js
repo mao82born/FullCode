a0fe1c16d891101f3e164cf41e79c8a39934e803
DiegoEdison <diegoedison05@gmail.com>
2022-11-20 23:21:50 -0500

    Actualizano para subir a heroku

--- backend/server.js
+++ backend/server.js
@@ -40,5 +40,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`serve at http;//localhost:${port}`);
+    console.log(`serve at http://localhost:${port}`);
 });
